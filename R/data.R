6090cd88adb51d00116cdda923c75f8afb613524
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2022-07-27 11:33:48 +0200

    Add labels_archive to package data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Label's archive
+#'
+#' Medium's statistics engine doesn't export labels.
+#' We read them from our rss feed.
+#'
+#' Unfortunately, the rss only stores the last blogposts.
+#' So we use this archive to keep track of older blogposts' labels.
+#'
+#' @format A data frame
+#' @source https://blog.esciencecenter.nl/feed
+"labels_archive"
